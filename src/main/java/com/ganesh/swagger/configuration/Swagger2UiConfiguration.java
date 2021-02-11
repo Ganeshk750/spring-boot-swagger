a5d0e40fcd1ffb61e2b36020e7b331e0b5feba4a
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-02-11 18:19:14 +0530

    # adding Swagger2UiConfiguration removing un-used import

--- src/main/java/com/ganesh/swagger/configuration/Swagger2UiConfiguration.java
+++ src/main/java/com/ganesh/swagger/configuration/Swagger2UiConfiguration.java
@@ -8,7 +8,6 @@ package com.ganesh.swagger.configuration;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
